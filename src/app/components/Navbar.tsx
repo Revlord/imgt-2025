483d1add62f9dd7fe42b92f651dd9c48578bd461
Revanth Reddy <revanthreddy4666@gmail.com>
2025-02-23 00:36:36 -0500

    navbar tranlucency fix

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
             className={`
                 fixed top-0 w-full z-50 transition-all duration-300
                 ${scrolled 
-                    ? 'bg-[#200630] backdrop-blur-md shadow-lg' 
+                    ? 'bg-[#200630]/70 backdrop-blur-md shadow-lg' 
                     : 'bg-transparent'
                 }
             `}
@@ -109,7 +109,7 @@ const Navbar = () => {
                     </div>
                 </div>
             </div>
-            
+
             {/* Mobile menu */}
             <div
                 className={`
